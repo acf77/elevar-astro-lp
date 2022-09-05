4fab42c12df4a7afe000e7b430dcdd2b15c215a5
Antonio Carlos Filho <antoniocarlos.j77@gmail.com>
2022-09-04 21:49:18 -0300

    added question to hero image

--- src/components/Text/CaptionText/styles.ts
+++ src/components/Text/CaptionText/styles.ts
@@ -8,3 +8,8 @@ export const captionTextStyleHeavy = {
   ...captionTextStyle,
   fontWeight: "600",
 };
+export const captionTextStyleLight = {
+  ...captionTextStyle,
+  fontWeight: "400",
+  fontSize: "14px",
+};
